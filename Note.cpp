a064ccc81e82b324a9bd3138281cef3a313e20cd
prathibanagarajan <prathib.arajan@gmail.com>
2016-08-25 14:32:25 -0700

    bugfixes

--- Note.cpp
+++ Note.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include <map>
 
+//declaring the basic notes
 const std::string Note::basicNotes[7] = { "S", "R", "G", "M", "P", "D", "N" };
 
+//Name: setNoteLevels()
+//Set values according to the order in a particular octave, in MIDI, according to the mapping to Western Music
 //Ref : http://www.electronics.dit.ie/staff/tscarff/Music_technology/midi/midi_note_numbers_for_octaves.htm
 //Ref : http://www.hitxp.com/keyboard-music-notes/western-notations-guide-convert-classical-indian-carnatic/
-
 void Note::setNoteLevels(){
 	noteLevels["S"] = 0;
 	noteLevels["R1"] = 1;
@@ -26,7 +28,8 @@ void Note::setNoteLevels(){
 	noteLevels["N3"] = 11;
 }
 
-
+//Constructor Function
+//Basic Notes declared and a default note is set
 Note::Note(void){
 	if (noteLevels.size() == 0){
 		setNoteLevels();
@@ -34,14 +37,20 @@ Note::Note(void){
 	newNote = "S"; //default
 }
 
+//Name: setNote(note)
+//The note object is set to a particular note 
 void Note::setNote(std::string note){
 	newNote = note;
 }
 
+//Name: getNote()
+//Return the note set in a note object
 std::string Note::getNote(){
 	return newNote;
 }
 
+//Name: getNoteLevel(note)
+//Returns the MIDI level of a particular note in a general octave
 int Note::getNoteLevel(std::string note){
 	if (noteLevels.count(note))
 		return noteLevels[note];
